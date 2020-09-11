5a0f70264c47e6047b377ce99250090c0c231848
Shawn <shawntaylor@weecology.org>
2020-09-11 16:22:15 -0600

    make helper function file

--- create_roi_base.R
+++ create_roi_base.R
@@ -2,25 +2,16 @@ library(tidyverse)
 library(sf)
 
 #######################
-# This script randomly locates the approate sized cells at each site.
-# These are not the ROIs used in the analysis, they are still
-# placed by hand using qgis
+# This script randomly locates the appropriate sized cells at each site.
+# These are not the ROIs used in the analysis, but are used as a template
+# to hand place everything on the correct plants using qgis.
 #######################
-
-make_cell_coords = function(ll_x, ll_y, scale){
-  # Make x,y coordinates of a fully connected square.
-  coords = rbind(c(ll_x, ll_y),
-                 c(ll_x, ll_y + scale),
-                 c(ll_x + scale, ll_y + scale),
-                 c(ll_x+scale, ll_y),
-                 c(ll_x, ll_y))
-  return(coords)
-}
 
 
 ####################################
 boundaries = sf::st_read('data/gis/site_boundaries.geojson')
 source('roi_config.R')
+source('spatial_helper_functions.R')
 
 for(site in c('P9', 'GIBPE', 'NORT')){
   b = boundaries %>%
@@ -29,24 +20,16 @@ for(site in c('P9', 'GIBPE', 'NORT')){
   roi_info = primary_roi_info %>%
     filter(site_id == site)
   
-  
   for(i in 1:nrow(roi_info)){
     random_points = st_sample(b, size = roi_info$n_cells[i])
-    random_points = st_coordinates(random_points) %>% as_tibble()
     
-    coord_list = list()
-    for(cell_i in 1:nrow(random_points)){
-      cell_coords = make_cell_coords(ll_x = random_points$X[cell_i],
-                                     ll_y = random_points$Y[cell_i],
-                                     scale = roi_info$scale[i])
-      coord_list[[cell_i]] = cell_coords
-    }
+    cell_geoms = points_to_squares(random_points,
+                                   scale = roi_info$scale[i])
 
-    cell_geoms = lapply(coord_list, function(x) st_polygon(list(x)))
     cell_features = st_sf(scale = roi_info$scale[i], 
                           site  = site,
                           plant = roi_info$plant[i],
-                          geom  = st_sfc(cell_geoms),
+                          geom  = cell_geoms,
                           crs   = st_crs(boundaries),
                           precision = 1000)
     
@@ -61,6 +44,6 @@ for(site in c('P9', 'GIBPE', 'NORT')){
 # every roi gets a unique id
 all_rois$roi_id = 1:nrow(all_rois)
 
-st_write(all_rois, dsn = 'data/gis/site_rois_1_starter.geojson', driver='GeoJSON')
+st_write(all_rois, dsn = 'data/gis/site_rois_1_starter-2.geojson', driver='GeoJSON')
 
 
---
+++ spatial_helper_functions.R
@@ -0,0 +1,50 @@
+library(sf)
+
+
+make_polygon = function(ll_x, ll_y, scale){
+  # Make a square geom given the lower left
+  # coordinates and size.
+  coords = rbind(c(ll_x, ll_y),
+                 c(ll_x, ll_y + scale),
+                 c(ll_x + scale, ll_y + scale),
+                 c(ll_x+scale, ll_y),
+                 c(ll_x, ll_y))
+  return(st_polygon(list(coords)))
+}
+
+points_to_squares = function(points, scale){
+  # Given an sf object of points, create a new 
+  # object of the same number of squares where the
+  # point represents the lower left corner. 
+  points = as.data.frame(st_coordinates(points))
+  cell_geoms = list()
+  for(i in 1:nrow(points)){
+    cell_geoms[[i]] = make_polygon(ll_x = points$X[i],
+                                   ll_y = points$Y[i],
+                                   scale = scale)
+  }
+  
+  return(st_sfc(cell_geoms, crs = st_crs(points)))
+}
+
+polygon_sample = function(p, n, new_polygon_scale){
+  # Within the polygon p, create n new square polygons
+  # of size new_polygon_scale
+  bbox = st_bbox(p)
+  
+  # Make a slightly cropped polygon to sample random points in
+  # so that the new subset cells do not bleed outside it.
+  cropped_scale = (bbox$xmax - bbox$xmin) - new_polygon_scale
+  search_space = make_polygon(ll_x = bbox$xmin,
+                              ll_y = bbox$ymin,
+                              scale = cropped_scale)
+  
+  random_points = st_sample(search_space, size=n)
+  subcell_geoms = points_to_squares(random_points,
+                                   scale = new_polygon_scale)
+  subcell_features = st_sf(geom  = subcell_geoms,
+                           crs   = st_crs(p),
+                           precision = 1000)
+  
+  return(subcell_features)
+}
