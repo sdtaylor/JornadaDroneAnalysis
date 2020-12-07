103eb45ee91e4420e20523422479345de02179c7
Shawn <shawntaylor@weecology.org>
2020-12-07 09:26:02 -0700

    increase text size

--- conceptual_figure.R
+++ conceptual_figure.R
@@ -54,8 +54,8 @@ doy_plant_30$plant_cubic_spline = predict(smooth.spline(doy_plant_30$doy, doy_pl
 
 
 ggplot(doy_plant_60, aes(x=doy)) + 
-  geom_line(aes(y=pure_endmember_curve), size=1.5) + 
-  scale_x_continuous(limits = c(1,365)) + 
+  geom_line(aes(y=pure_endmember_curve), size=2) + 
+  scale_x_continuous(limits = c(1,365), breaks=c(1,100,200,300)) + 
   theme_minimal() +
   labs(title = 'A. Plant Endmember VI Curve') + 
   theme(panel.grid = element_blank(),
@@ -64,9 +64,10 @@ ggplot(doy_plant_60, aes(x=doy)) +
         legend.text = element_text(size=14),
         legend.background = element_rect(color='black'),
         legend.key.width = unit(20,'mm'),
-        axis.text = element_text(size=10,color='black'),
+        axis.text = element_text(size=20,color='black'),
         axis.title = element_blank(),
-        strip.text = element_text(size=12, color='black', hjust=0.2))
+        plot.title = element_text(size=25, color='black'),
+        strip.text = element_text(size=50, color='black', hjust=0.2))
 
 both = doy_plant_30 %>%
   bind_rows(doy_plant_60) %>%
@@ -75,20 +76,20 @@ both = doy_plant_30 %>%
 both$curve_type = factor(both$curve_type, levels=c('plant_scaled','plant_cubic_spline'), labels=c('Scaled Pixel VI','Cubic Spline'))
 
 ggplot(both, aes(x=doy)) + 
-  geom_line(aes(y=curve_value, linetype=curve_type, color=curve_type), size=1.5) + 
+  geom_line(aes(y=curve_value, linetype=curve_type, color=curve_type), size=2) + 
   scale_linetype_manual(values=c('solid','solid')) + 
   scale_color_manual(values=c('#009E73','black')) + 
   geom_point(aes(y=plant_scaled_with_error), color='black', size=2) +
-  scale_x_continuous(limits = c(1,365)) + 
+  scale_x_continuous(limits = c(1,365),  breaks=c(1,100,200,300)) + 
   facet_wrap(~plant_cover, ncol = 1) +
   theme_minimal() +
   theme(panel.grid = element_blank(),
         legend.position = c(0.8,0.45),
         legend.title = element_blank(),
-        legend.text = element_text(size=14),
+        legend.text = element_text(size=20),
         legend.background = element_rect(color='black'),
         legend.key.width = unit(20,'mm'),
-        axis.text = element_text(size=10,color='black'),
+        axis.text = element_text(size=20,color='black'),
         axis.title = element_blank(),
-        strip.text = element_text(size=12, color='black', hjust=0.2))
+        strip.text = element_text(size=25, color='black', hjust=0.1, ))
  
