9347618c93206bb740eb4f1d3b2bb1cfd7def55b
Shawn <shawntaylor@weecology.org>
2021-03-17 11:38:18 -0600

    add supp figure for all ecoregions X veg classes

--- make_ecoregion_cover_timeseries_figs.R
+++ make_ecoregion_cover_timeseries_figs.R
@@ -6,8 +6,8 @@ rap_cover = read_csv('data/rap_cover.csv') %>%
   mutate(ecoregion = recode(ecoregion, 'Arizona/New Mexico Plateau'= 'Arizona-NM Plateau',
                                         'Sonoran Basin and Range' = 'Sonoran Desert')) %>%
   mutate(total_cover =  afag + pfag + shr + tree) %>%
-  select(ecoregion, point_id, year,total_cover) %>%
-  #pivot_longer(c(-ecoregion,-point_id,-year), names_to='cover_class', values_to='cover') %>%
+  #select(ecoregion, point_id, year,total_cover) %>%
+  pivot_longer(c(-ecoregion,-point_id,-year), names_to='cover_class', values_to='cover') %>%
   as_tibble()
 
 
@@ -24,17 +24,41 @@ rap_cover = read_csv('data/rap_cover.csv') %>%
 
 
 median_cover = rap_cover %>%
-  group_by(ecoregion, year) %>%
-  summarise(cover_median = median(total_cover),
-            cover_mean   = mean(total_cover),
-            cover_high   = quantile(total_cover, 0.975),
-            cover_low    = quantile(total_cover, 0.025),
+  group_by(ecoregion, year, cover_class) %>%
+  summarise(cover_median = median(cover),
+            cover_mean   = mean(cover),
+            cover_high   = quantile(cover, 0.975),
+            cover_low    = quantile(cover, 0.025),
             n=n()) %>%
   ungroup()
+
+cover_class_codes =           c('afag',                  'bg',         'ltr',   'pfag',                     'shr',   'tree', 'total_cover')
+cover_class_labels = str_wrap(c('Annual Forbs & Grasses','Bare Ground','Litter','Perennial Forbs & Grasses','Shrubs','Trees','Total Veg. Cover'),15)
+
+
+#---------------------
+# Large figure with each cover class and ecoregion
+median_cover %>%
+  mutate(cover_class = factor(cover_class, levels=cover_class_codes, labels=cover_class_labels, ordered = T)) %>%
+ggplot(aes(x=year, y=cover_median, color=cover_class)) + 
+  geom_errorbar(aes(ymin=cover_low, ymax=cover_high),size=1.5, width=0) +
+  geom_point(color='black', size=2) + 
+  scale_color_brewer(palette = 'Dark2') + 
+  facet_grid(str_wrap(ecoregion,12)~cover_class) +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.text = element_text(size=12),
+        axis.text = element_text(color='black')) + 
+  labs(x='Year', y= 'Fractional Cover')
+
+#---------------------
+# individual ecoregion timeseries using only total cover
+# these are put into the map using qgis.
 
 for(e in unique(median_cover$ecoregion)){
   
   fig =  median_cover %>%
+    filter(cover_class == 'total_cover') %>%
     filter(ecoregion==e) %>%
       ggplot(aes(x=year, y=cover_median)) + 
         #geom_line(linetype='dotted', size=4) +
