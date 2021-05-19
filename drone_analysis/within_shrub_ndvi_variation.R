86a99ee369f28f72cc7df95afc84c76673e3a4bd
Shawn <shawntaylor@weecology.org>
2021-05-19 17:17:33 -0600

    save fig S2 directly to file

--- drone_analysis/within_shrub_ndvi_variation.R
+++ drone_analysis/within_shrub_ndvi_variation.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 
+# This creates figure S2 showing the within shrub NDVI variation in the drone/UAV imagery.
+# Carefull running it as it takes a lot of memory in the upsample step.
+
 nort_shrubs = sf::st_read('./drone_analysis/data/gis/NORT_shrubs.geojson') %>%
   rename(shrub_id = polygon_id)  
 
-nort_ndvi_raster = raster::raster('./data/imagery/NORT_Jul5_2019_index_ndvi.tif')
+nort_ndvi_raster = raster::raster('drone_analysis/data/imagery/NORT_Jul5_2019_index_ndvi.tif')
 nort_ndvi_raster = raster::aggregate(nort_ndvi_raster, fact=5) # upsample to ~10cm pixels
 
 
@@ -22,13 +25,17 @@ mean_shrub_ndvi = x %>%
             median_ndvi = median(ndvi, na.rm=T)) %>%
   ungroup()
 
-x %>%
+figS2_shrub_ndvi_variation = x %>%
   filter(shrub_id %in% c(random_shrubs)) %>%
   ggplot(aes(x=ndvi)) + 
   #geom_density(fill='transparent', size=2) + 
   geom_histogram(binwidth = 0.02) + 
   geom_vline(data = mean_shrub_ndvi, aes(xintercept=mean_ndvi), size=1.5, color='#d55e00') + 
   facet_wrap(~shrub_id, labeller = label_both) + 
-  theme_bw(12) + 
-  theme(axis.text = element_text(color='black')) +
+  theme_bw(14) + 
+  theme(axis.text = element_text(color='black'),
+        strip.text = element_text(size=14),
+        strip.background = element_blank()) +
   labs(x='NDVI', y='Count of 10cm pixels')
+
+ggsave('manuscript/figS2_shrub_ndvi_variation.png', plot=figS2_shrub_ndvi_variation, height=20, width=20, units='cm', dpi=150)
